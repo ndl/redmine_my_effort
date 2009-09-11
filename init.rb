604598aaaeeb87b76ce1c99b93a86e2c16d8b17b
Ricardo Duarte <ricardo@ricardoduarte.com>
2009-09-11 12:49:11 -0300

    Translating plugin name in Menu

--- init.rb
+++ init.rb
@@ -7,10 +7,11 @@ Redmine::Plugin.register :redmine_my_effort do
   version '0.1.0'
 
   permission :my_effort, {:user_efforts => [:index, :new, :stop]}, :require => :member
-  menu :top_menu, :my_effort, { :controller => 'user_efforts', :action => 'index' }, :caption => 'My Effort'
+  menu :top_menu, :my_effort, { :controller => 'user_efforts', :action => 'index' }, :caption => :my_effort_title
 
   settings :default => {
     'tick_interval' => '30'
   }, :partial => 'settings/myeffort_settings'
 
 end
+
