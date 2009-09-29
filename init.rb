50f51b3cc9dd5be0264dbd5996ba3837c12791bf
Ricardo Duarte <ricardo@ricardoduarte.com>
2009-09-29 20:20:31 -0300

    Revert "Added Plugin name and description to .yml translation files"
    This reverts commit eb1f8afaabfaa80c8540266c5dfd15cf7fe4c74a.

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_my_effort do
-  name :plugin_name
+  name 'My Effort plugin'
   author 'Oleg Vivtash'
-  description :plugin_description
+  description 'Allows to start/stop timers for time spent on an issue from a separate tab'
   version '0.1.0'
 
   permission :my_effort, {:user_efforts => [:index, :new, :stop]}, :require => :member
