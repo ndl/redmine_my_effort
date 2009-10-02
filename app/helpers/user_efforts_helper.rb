1b01eeb9578ee61cd14d3dd4b25c32b268fdd564
Lucio Benfante <redmine@lnx1.LB3515.it>
2009-10-02 22:48:08 +0800

    Fixed for working with the most recent redmine API.

--- app/helpers/user_efforts_helper.rb
+++ app/helpers/user_efforts_helper.rb
@@ -1,7 +1,7 @@
 module UserEffortsHelper
 
   def activity_collection_for_select_options
-    activities = Enumeration::get_values('ACTI')
+    activities = TimeEntryActivity.all
     collection = []
     collection << [ "--- #{l(:actionview_instancetag_blank_option)} ---", '' ] unless activities.detect(&:is_default)
     activities.each { |a| collection << [a.name, a.id] }
