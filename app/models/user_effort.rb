81054b343115c776b46f14d9984c05c861776bd2
root <root@ndl-server.ndl.kiev.ua>
2009-10-12 20:35:17 +0800

    - Changed hours_spent calculation to use standard DateTime functionality.

--- app/models/user_effort.rb
+++ app/models/user_effort.rb
@@ -13,25 +13,10 @@ class UserEffort < ActiveRecord::Base
   end
 
   def hours_spent
-    today = DateTime.now
-    if today.hour < start_time.hour
-      hours = (today.day - start_time.day - 1)*24 + today.hour + (24 - start_time.hour)
-    elsif today.hour == start_time.hour+1
-      hours = 0
-    else
-      hours = today.hour - start_time.hour
-    end
-    if today.min < start_time.min
-      minutes = (60-start_time.min) + today.min
-    else
-      minutes = today.min - start_time.min
-    end
-
+    hours, minutes = Date.day_fraction_to_time(DateTime.now - start_time.to_datetime)
+    minutes += hours * 60
     rtick = (Setting.plugin_redmine_my_effort['tick_interval']).to_i
-    dh = rtick * (minutes / rtick)
-    dh += rtick if minutes-dh > rtick/2
-
-    (hours+dh/60.0).to_f
+    rtick * (minutes.to_f / rtick.to_f).round / 60.0
   end
 
 end
