f2aee2499051b07c0c9b6c8317ea20ed4d8165b4
root <root@ndl-server.ndl.kiev.ua>
2009-10-12 20:36:25 +0800

    - Stop timer when issue status is changed to closed one (either via manual update or automatically on commit).

---
+++ lib/my_effort_issue_hooks.rb
@@ -0,0 +1,47 @@
+require 'redmine'
+
+class MyEffortIssueHook < Redmine::Hook::Listener
+  
+  def controller_issues_edit_before_save(context)
+    stop_timer(context)
+  end
+
+  def model_changeset_scan_commit_for_issue_ids_pre_issue_update(context)
+    stop_timer(context)
+  end
+
+  private
+
+  def stop_timer(context)
+    begin
+      issue = context[:issue]
+      old_issue = Issue.find(issue.id)
+      time_entry = context[:time_entry]
+      # issue.status might be not valid at this point, so we have to
+      # use status_id instead.
+      closed = IssueStatus.find(issue.status_id).is_closed?
+      if old_issue and not old_issue.closed? and closed
+        efforts = UserEffort.find(:all, :conditions => {:issue_id => issue.id})
+        efforts.each do |effort|
+          if not time_entry
+            time_entry = TimeEntry.new
+            time_entry.user = effort.user
+            time_entry.issue = issue
+            time_entry.project = issue.project if issue.project_id
+            time_entry.activity = Enumeration.find(:first, :conditions => {:opt => "ACTI"})
+            time_entry.spent_on = Time.now.to_date
+            time_entry.hours = effort.hours_spent
+            time_entry.save
+          else
+            if not time_entry.hours
+              time_entry.hours = effort.hours_spent
+            end
+          end
+          effort.destroy
+        end
+      end
+    rescue
+      Rails.logger.error("Error in stop_timer: #{$!}")
+    end
+  end
+end
